aa298850d9bfe04d4e0dd8a31d65c7c4b9681407
Brendan <metroid.dreadnaught@gmail.com>
2026-03-16 16:57:15 -0400

    Project Hangman Debug WIP

--- Homework/HW2/Hangman Debug.py
+++ Homework/HW2/Hangman Debug.py
@@ -1,63 +1,7 @@
-# Name:
-# Section:
-# 6.189 Project 1: Hangman template
-# hangman_template.py
 
-# Import statements: DO NOT delete these! DO NOT write code above this!
 from random import randrange
 from string import *
-
-# -----------------------------------
-# Helper code
-# (you don't need to understand this helper code)
-# Import hangman words
-
 WORDLIST_FILENAME = "words.txt"
-
-
-def load_words():
-    """
-    Returns a list of valid words. Words are strings of lowercase letters.
-
-    Depending on the size of the word list, this function may
-    take a while to finish.
-    """
-"""    print
-    "Loading word list from file..."
-    # inFile: file
-    inFile = open(WORDLIST_FILENAME, 'r', 0)
-    # line: string
-    line = inFile.readline()
-    # wordlist: list of strings
-    wordlist = line.split()
-    print
-    "  ", len(wordlist), "words loaded."
-    print
-    'Enter play_hangman() to play a game of hangman!'
-    return wordlist"""
-
-
-# actually load the dictionary of words and point to it with
-# the words_dict variable so that it can be accessed from anywhere
-# in the program
-# words_dict = load_words()
-
-
-# Run get_word() within your program to generate a random secret word
-# by using a line like this within your program:
-# secret_word = get_word()
-
-def get_word():
-    """
-    Returns a random word from the word list
-    """
-#    word = words_dict[randrange(0, len(words_dict))]
-#    return word
-
-
-# end of helper code
-# -----------------------------------
-
 
 # CONSTANTS
 MAX_GUESSES = 6
@@ -101,9 +45,7 @@ for i in List1:
 print(string1.lower())"""
 
 def print_guessed(secret_word, letters_guessed):
-    '''
-    Prints out the characters you have guessed in the secret word so far
-    '''
+
     # global secret_word
     # global letters_guessed
     this_try = []
@@ -130,6 +72,11 @@ test2 = print_guessed("claptrap", ['a','p'])
 assert test2 == '--ap--ap', 'test2 was ' + test2
 test3 = print_guessed("claptrap", ['a','l','m','c','e','t','r','p','n'])
 assert test3 == 'claptrap', 'test3 was ' + test3
+
+global secret_word
+global letters_guessed
+global mistakes_made
+global MAX_GUESSES
 
 def play_hangman():
     # Actually play the hangman game
